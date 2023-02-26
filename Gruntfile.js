3ed06fa9f27026fe46ec2b7430d8bf15fee6c652
Simon Paschold <s.paschold@pixelstein.de>
2023-02-26 22:22:57 +0100

    init

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ['@babel/env'],
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: [
+              '**/*.js',
+            ],
+            dest: './dist/',
+            ext: '.js',
+          }],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-vueify');
+  grunt.loadNpmTasks('grunt-babel');
+
+  grunt.registerTask('default', ['babel']);
+};
